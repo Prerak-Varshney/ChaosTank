9856288dc13fb666b608b90da90201f9def3b346
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-09-14 18:45:53 +0530

    Minor Change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     ENV: 'production',
-    API_URL: process.env.ENV === 'development' ? 'http://localhost:3001/api/v1/water-level' : 'https://apichaostank.vercel.app/api/v1/',
+    API_URL: process.env.ENV === 'development' ? 'http://localhost:3001/api/v1/water-level' : 'https://apichaostank.vercel.app/api/v1',
   }
 };
 
