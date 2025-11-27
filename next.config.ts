ad3a4726b0acd4ce3e81bfa8e1a05ede8d5fccad
Prerak Varshney <prerak.var@gmail.com>
2025-11-27 18:33:15 +0530

    Test for ios headers'

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,38 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     async rewrites() {
-        const apiUrl = process.env.API_URL || "http://localhost:3001";
-        console.log(`Rewriting /api/v1 to ${apiUrl}`);
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+
         return [
             {
                 source: "/api/v1/:path*",
                 destination: `${apiUrl}/api/v1/:path*`,
+            },
+        ];
+    },
+
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://chaostank.vercel.app", // your client domain
+                    },
+                    {
+                        key: "Access-Control-Allow-Credentials",
+                        value: "true",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PUT,DELETE,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
             },
         ];
     },
